4e529b384ef02727b5bd04818005c487c1c13b76
Jazmin M <jazminmerckel@gmail.com>
2019-10-31 04:28:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,28 +3,21 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  
+  extend Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable 
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
