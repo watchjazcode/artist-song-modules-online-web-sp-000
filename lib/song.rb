9a0cbf6f479c7ead843b4fa49347c48992d9ef67
Jazmin M <jazminmerckel@gmail.com>
2019-10-31 04:23:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,8 @@ class Song
   attr_accessor :name
   attr_reader :artist
   
-  extend Memorable
-  extend Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
 
   @@songs = []
 
